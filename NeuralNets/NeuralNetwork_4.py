5108b48caeb3e2ab831d7312b3213b9ecbea37a9
Axtya Darius <barbanod@bxscience.edu>
2017-12-02 16:00:22 -0500

    made python code more user-friendly/added some examples

--- NeuralNets/NeuralNetwork_4.py
+++ NeuralNets/NeuralNetwork_4.py
@@ -1,7 +1,12 @@
+#This code defines an abstract syntax tree as a binary tree
+
 class Node:
     leftChild = None
     rightChild = None
     op = ""
+
+    def __init__(self):
+        return
 
     def __init__(self, o = 0, left = None, right = None):
         self.op = o
@@ -9,14 +14,48 @@ class Node:
         self.rightChild = right
         return
 
-    def eval(self):
-        if self.op.isdigit():
-            return self.op
-        elif self.leftChild == None or self.rightChild == None:
+    def eval(self): #evaluate the tree by moving down the nodes and evaluating whenever there is a literal expression
+        if self.rightChild == None and self.leftChild == None:
             return self.op
         else:
-            return str(eval(self.leftChild.eval() + self.op + self.rightChild.eval()))
+            if not self.rightChild == None:
+                return str(eval(self.leftChild.eval() + " " + self.op + " " + self.rightChild.eval()))
+            else:
+                return str(eval(self.op + " " + self.leftChild.eval()))
 
-node = Node("*", Node("-",Node("3"),Node("4")), Node("/",Node("4"),Node("2"))) #print result of (3 - 4) * (4 / 2)
+def BuildTree(lst): #helper function to create a full syntax tree from a list using Node objects
+    source = Node()
+    source.op = lst[0]
 
-print node.eval();
+    if len(lst) == 2 and len(lst[1]) > 1:
+        source.leftChild = BuildTree(lst[1])
+        source.rightChild = None
+        return source
+    elif len(lst) == 2 and len(lst[1]) == 1:
+        source.leftChild = Node(lst[1][0],None,None)
+        source.rightChild = None
+        return source
+
+    if len(lst[1]) == 1:
+        source.leftChild = Node(lst[1][0],None,None)
+    else:
+        source.leftChild = BuildTree(lst[1])
+
+    if len(lst[2]) == 1:
+        source.rightChild = Node(lst[2][0],None,None)
+    else:
+        source.rightChild = BuildTree(lst[2])
+
+    return source
+
+
+
+#simple arithmetic:
+arithmeticTree = ["*", ["-",["3"],["5"]], ["/",["4"],["2"]]] #outputs -4
+arithmeticTree = BuildTree(arithmeticTree);
+print arithmeticTree.eval();
+
+#simple logic:
+logicalTree = ["not",["and", ["or",["True"],["False"]], ["and",["False"],["True"]]]] #outputs True
+logicalTree = BuildTree(logicalTree);
+print logicalTree.eval();
