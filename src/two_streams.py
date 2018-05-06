5db16b4592bb51a56b701046ada4f3a408f2f6ce
DariusBxsci <barbanod@bxscience.edu>
2018-05-06 10:02:45 -0400

    fixed code for python3

--- src/two_streams.py
+++ src/two_streams.py
@@ -18,21 +18,21 @@ associative_layer = np.zeros((N["vertical"]['associative layer'],))
 sparsity = 0.25
 
 patterns_to_recognize = np.array([np.random.choice([0,1],size = hidden_layers.size,p=[1-sparsity,sparsity])
-									for _ in xrange(N["patterns"])])
+									for _ in range(N["patterns"])])
 
 
 starting_input = patterns_to_recognize[0]
 M = 1.25/((1-sparsity)*sparsity*hidden_layers.size)*np.sum([np.outer(pattern-sparsity*np.ones((pattern.shape)),pattern-sparsity*np.ones((pattern.shape)))
 																for pattern in patterns_to_recognize],axis=-1)	
 
-print M
+print (M)
 M = np.divide(M,1./(sparsity*hidden_layers.size)*np.ones(M.shape))
 W = np.linalg.pinv(patterns_to_recognize.T)
-for _ in xrange(10): #Just a few time steps
-	print 'Hidden Layer Activity. ',patterns_to_recognize[0,:]
-	print 'Feedforwward Contribution of HLA. ', W.dot(patterns_to_recognize[0,:])
+for _ in range(10): #Just a few time steps
+	print ('Hidden Layer Activity. ',patterns_to_recognize[0,:])
+	print ('Feedforwward Contribution of HLA. ', W.dot(patterns_to_recognize[0,:]))
 
-	print M.T.dot(associative_layer)
+	print (M.T.dot(associative_layer))
 
 '''
 print xinv.dot(patterns_to_recognize[1,:])
